40ae90c8319a3e28c866c718e1cf357e1e26c41a
saniya anand <sa2225@afsconnect1.njit.edu>
2017-12-07 20:58:19 -0500

    login using bcrypt

--- collections/accounts.php
+++ collections/accounts.php
@@ -4,10 +4,11 @@ class accounts extends \database\collection
     protected static $modelName = 'account';
     //This is the function to write to find user by ID for login.
     //Don't forget to return the object see findOne in the collection class
-    public static function findUserbyEmail($email)
+    public static function findUserbyUsername($uname)
     {
             $tableName = get_called_class();
-            $sql = 'SELECT * FROM ' . $tableName . ' WHERE email = ?';
+            $sql = 'SELECT * FROM ' . $tableName . ' WHERE email="' . $uname.'"';
+            echo $sql;
          //grab the only record for find one and return as an object
             $recordsSet = self::getResults($sql, $email);
             if (is_null($recordsSet)) {
@@ -17,3 +18,4 @@ class accounts extends \database\collection
             }
     }
 }
+?>
