dc03a0feb22a738dcd312990661039298d03d285
saniya anand <sa2225@afsconnect1.njit.edu>
2017-12-10 02:10:24 -0500

    minor changes

--- pages/homepage.php
+++ pages/homepage.php
@@ -24,19 +24,18 @@
     echo $data['site_name'];
     ?> </h1>
 
-<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
-<h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>
+  <h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
+  <h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>
 
 <form action="index.php?page=accounts&action=login" method="POST">
 
     <div class="container">
         <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="email" required>
-
+        <input type="text" placeholder="Enter Username" name="email" required><br><br>
         <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
+        <input type="password" placeholder="Enter Password" name="password" required><br><br>
 
-        <button type="submit">Login</button>
+        <button type="submit">Login</button><br><br>
     </div>
 
 
