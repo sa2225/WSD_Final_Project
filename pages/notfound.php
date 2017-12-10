811cf182d180cdff4414c1ad387e45e17c222fb5
saniya anand <sa2225@afsconnect1.njit.edu>
2017-12-10 02:31:08 -0500

    minor changes

--- pages/notfound.php
+++ pages/notfound.php
@@ -4,11 +4,8 @@
 <head>
     <meta charset="utf-8">
 
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
-
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
+    <title>My Final Project</title>
+	<link rel="stylesheet" href="css/styles.css?v=1.0">
 
     <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
@@ -17,7 +14,7 @@
 
 <body>
 
-<h1>Page Not Found 404</h1>
+<h1><b>Page Not Found 404</b>></h1>
 
 
 <script src="js/scripts.js"></script>
