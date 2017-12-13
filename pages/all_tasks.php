0a3165716bf620f4a4faa61b9fdea24aff0ce5dd
saniya anand <sa2225@afsconnect1.njit.edu>
2017-12-12 19:25:13 -0500

    modified code

--- pages/all_tasks.php
+++ pages/all_tasks.php
@@ -1,36 +1,53 @@
 <!doctype html>
 
+
+
 <html lang="en">
+
 <head>
+
     <meta charset="utf-8">
 
-    <title>My Final Project</title>
+
+
+    <title>My WSD Project</title>
+
+
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
 
+
+
     <!--[if lt IE 9]>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+
     <![endif]-->
+
 </head>
 
+
+
 <body>
-	<form action="index.php?page=accounts&action=logout" method="post" id="form3">
 
-			<button type="submit" form="form3" value="logout">Logout</button>
 
-</form>
 
 <?php
+
 //this is how you print something
+
+//print_r($data);
+
 print utility\htmlTable::genarateTableFromMultiArray($data);
+
 ?>
 
-<form action="index.php?page=tasks&action=create" method="POST">
-		<div class="container">
-				<label><b>Add a task:</b></label><button type="submit">Add Task</button>
-		</div>
-</form>
+
+
+
 
 <script src="js/scripts.js"></script>
+
 </body>
+
 </html>
